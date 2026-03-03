d4070c1be763801f05497843112379178b6ea4b1
Hatem <hatemswify@gmail.com>
2026-03-03 22:36:13 +0200

    Feature: Store form data in JSON file

--- done.php
+++ done.php
@@ -9,13 +9,48 @@
 
     <?php
 
-    // Get data from GET
-    $fname = $_GET['fname'];
-    $lname = $_GET['lname'];
-    $address = $_GET['address'];
-    $gender = $_GET['gender'];
-    $skills = $_GET['skills'];
-    $department = $_GET['department'];
+    // Get data from POST
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $address = $_POST['address'];
+    $country = $_POST['country'];
+    $gender = $_POST['gender'];
+    $skills = $_POST['skills'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $department = $_POST['department'];
+    $code = $_POST['code'];
+
+    // Store data in a file
+    $data = [
+        'id' => uniqid(),
+        'timestamp' => date('Y-m-d H:i:s'),
+        'fname' => $fname,
+        'lname' => $lname,
+        'address' => $address,
+        'country' => $country,
+        'gender' => $gender,
+        'skills' => $skills,
+        'username' => $username,
+        'password' => $password,
+        'department' => $department,
+        'code' => $code
+    ];
+
+    $file = 'data.json';
+    $currentData = [];
+    
+    // Read existing data if file exists
+    if (file_exists($file)) {
+        $jsonData = file_get_contents($file);
+        $currentData = json_decode($jsonData, true) ?: [];
+    }
+    
+    // Add new data
+    $currentData[] = $data;
+    
+    // Save back to file
+    file_put_contents($file, json_encode($currentData, JSON_PRETTY_PRINT));
 
     // Title based on gender
     if ($gender == "Female") {
@@ -56,6 +91,11 @@
     <p>
         <strong>Department:</strong>
         <?php echo $department; ?>
+    </p>
+
+    <p>
+        <strong>Data saved successfully!</strong><br>
+        <a href="list_data.php">View All Registrations</a>
     </p>
 
 </body>
